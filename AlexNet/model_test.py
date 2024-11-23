8dbdb48ebfa307430a56b2f9ac0ec3067d1e29a0
SitingT <tang.sit@northeastern.edu>
2024-11-22 16:01:49 -0800

    Alenxt building and test

--- AlexNet/model_test.py
+++ AlexNet/model_test.py
@@ -2,20 +2,20 @@ import torch
 import torch.utils.data as Data
 from torchvision import transforms
 from torchvision.datasets import FashionMNIST
-from model import LeNet
+from model import AlexNet
+
 
 
 def test_data_process():
     test_data = FashionMNIST(root='./data',
-                             train=False,
-                             transform=transforms.Compose(
-                                 [transforms.Resize(size=28), transforms.ToTensor()]),
-                             download=True)
+                              train=False,
+                              transform=transforms.Compose([transforms.Resize(size=227), transforms.ToTensor()]),
+                              download=True)
 
     test_dataloader = Data.DataLoader(dataset=test_data,
-                                      batch_size=1,
-                                      shuffle=True,
-                                      num_workers=0)
+                                       batch_size=1,
+                                       shuffle=True,
+                                       num_workers=0)
     return test_dataloader
 
 
@@ -40,7 +40,7 @@ def test_model_process(model, test_dataloader):
             # 设置模型为评估模式
             model.eval()
             # 前向传播过程，输入为测试数据集，输出为对每个样本的预测值
-            output = model(test_data_x)
+            output= model(test_data_x)
             # 查找每一行中最大值对应的行标
             pre_lab = torch.argmax(output, dim=1)
             # 如果预测正确，则准确度test_corrects加1
@@ -55,29 +55,35 @@ def test_model_process(model, test_dataloader):
 
 if __name__ == "__main__":
     # 加载模型
-    model = LeNet()
-    model.load_state_dict(torch.load(
-        '/Users/sitingtang/Desktop/coop-fall/LeNet_AlexNet/LeNet/model_weights.pth'))
-    # 加载测试数据
+    model = AlexNet()
+    model.load_state_dict(torch.load('best_model.pth'))
+    # # 利用现有的模型进行模型的测试
     test_dataloader = test_data_process()
-    # 加载模型测试的函数
     test_model_process(model, test_dataloader)
 
-    # device = "cuda" if torch.cuda.is_available() else 'cpu'
-    # model = model.to(device)
 
-    # classes = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress',
-    #            'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
-    # with torch.no_grad():
-    #     for b_x, b_y in test_dataloader:
-    #         b_x = b_x.to(device)
-    #         b_y = b_y.to(device)
+    # 设定测试所用到的设备，有GPU用GPU没有GPU用CPU
+    device = "cuda" if torch.cuda.is_available() else 'cpu'
+    model = model.to(device)
 
-    #         # 设置验证模式
-    #         model.eval()
-    #         output = model(b_x)  # 十个神经推理结果
-    #         pre_lab = torch.argmax(output, dim=1)
-    #         result = pre_lab.item()
-    #         label = b_y.item()
-    #         print("Expected", classes[result],
-    #               "-------", "real: ", classes[label])
+    classes = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+    with torch.no_grad():
+        for b_x, b_y in test_dataloader:
+            b_x = b_x.to(device)
+            b_y = b_y.to(device)
+
+            # 设置模型为验证模型
+            model.eval()
+            output = model(b_x)
+            pre_lab = torch.argmax(output, dim=1)
+            result = pre_lab.item()
+            label = b_y.item()
+            print("预测值：",  classes[result], "------", "真实值：", classes[label])
+
+
+
+
+
+
+
+
